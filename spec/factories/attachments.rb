306653788cac1be50508ebed7409c32f8b4d951c
Andrew <andreymg184@mail.com>
2023-04-03 18:15:20 +0600

    add attachment removal for question

--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :attachment do
-    file { "MyString" }
+    file { File.open("#{Rails.root}/spec/fixtures/test_file.txt") }
+    
+    trait :for_question do
+      association :attachable, factory: :question
+    end
   end
 end
